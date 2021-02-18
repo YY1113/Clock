1e0866f94102c726382fe4e01c955fc0669ac1ba
YY1113 <75839430+YY1113@users.noreply.github.com>
2021-02-18 21:41:58 +0900

    wow!!!

--- os.py
+++ os.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
+import random
+
 
 subprocess.call("git add *")
-subprocess.call('git commit -m"4test"')
+print("커밋 메시지를 입력해주세요")
+message=input()
+subprocess.call('git commit -m'+'"'+message+'"')
 subprocess.call("git push origin master")
 
 
